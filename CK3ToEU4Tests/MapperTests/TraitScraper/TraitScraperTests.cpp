9916e5ea0a1ea3a056a0bbd13a19ac2c72b2f8a6
Ivo Elezovic <ivo@elezovic.su>
2020-09-13 16:16:44 +0200

    more tests.

--- CK3ToEU4Tests/MapperTests/TraitScraper/TraitScraperTests.cpp
+++ CK3ToEU4Tests/MapperTests/TraitScraper/TraitScraperTests.cpp
@@ -52,6 +52,28 @@ TEST(Mappers_TraitScraperTests, traitsWithIndexAreLoaded)
 	ASSERT_EQ("trait3", traitScraper.getTraits().find(8)->second);
 }
 
+TEST(Mappers_TraitScraperTests, traitsCanBeOverriddenByMods)
+{
+	std::stringstream input;
+	input << "trait1 = { index = 4 }\n";
+	input << "trait2 = { index = 6 }\n";
+	input << "trait3 = { index = 8 }\n";
+
+	mappers::TraitScraper traitScraper;
+	traitScraper.loadTraits(input);
+
+	std::stringstream input2;
+	input2 << "nutrait1 = { index = 4 }\n";
+	input2 << "nutrait2 = { index = 6 }\n";
+	input2 << "nutrait3 = { index = 8 }\n";
+	traitScraper.loadTraits(input2);
+	
+	ASSERT_EQ(3, traitScraper.getTraits().size());
+	ASSERT_EQ("nutrait1", traitScraper.getTraits().find(4)->second);
+	ASSERT_EQ("nutrait2", traitScraper.getTraits().find(6)->second);
+	ASSERT_EQ("nutrait3", traitScraper.getTraits().find(8)->second);
+}
+
 TEST(Mappers_TraitScraperTests, traitsCanMatchForID)
 {
 	std::stringstream input;
