bd69fab262a2ebeb84e7fb3398d37b7f5b57a291
Ivo Elezovic <ivo@elezovic.su>
2020-09-15 19:51:17 +0200

    rakaly update

--- CK3toEU4/Source/Helpers/rakaly_wrapper.h
+++ CK3toEU4/Source/Helpers/rakaly_wrapper.h
@@ -1,46 +1,40 @@
 #ifndef RAKALY_WRAPPER_H
 #define RAKALY_WRAPPER_H
 
+#include "rakaly.h"
 #include <stdexcept>
 #include <string>
-#include "rakaly.h"
 
-namespace rakaly
-{
+namespace rakaly {
 
-inline std::string meltFinish(MeltedBuffer * melt)
-{
-	int result = rakaly_melt_error_code(melt);
-	if (result)
-	{
-		rakaly_free_melt(melt);
-		throw std::runtime_error("librakaly returned an error: " + std::to_string(result));
-	}
+inline std::string meltFinish(MeltedBuffer *melt) {
+  int result = rakaly_melt_error_code(melt);
+  if (result) {
+    rakaly_free_melt(melt);
+    throw std::runtime_error("librakaly returned an error: " +
+                             std::to_string(result));
+  }
 
-	size_t len = rakaly_melt_data_length(melt);
-	std::string str(len, ' ');
+  size_t len = rakaly_melt_data_length(melt);
+  std::string str(len, ' ');
 
-	if (rakaly_melt_write_data(melt, str.data(), len) != len)
-	{
-		rakaly_free_melt(melt);
-		throw std::runtime_error("librakaly failed to copy data.");
-	}
+  if (rakaly_melt_write_data(melt, str.data(), len) != len) {
+    rakaly_free_melt(melt);
+    throw std::runtime_error("librakaly failed to copy data.");
+  }
 
-	rakaly_free_melt(melt);
-	return str;
+  rakaly_free_melt(melt);
+  return str;
 }
 
-inline std::string meltEU4(const std::string& encrypted)
-{
-	return meltFinish(rakaly_eu4_melt(encrypted.c_str(), encrypted.length()));
+inline std::string meltEU4(const std::string &encrypted) {
+  return meltFinish(rakaly_eu4_melt(encrypted.c_str(), encrypted.length()));
 }
 
-inline std::string meltCK3(const std::string& encrypted)
-{
-	return meltFinish(rakaly_ck3_melt(encrypted.c_str(), encrypted.length()));
+inline std::string meltCK3(const std::string &encrypted) {
+  return meltFinish(rakaly_ck3_melt(encrypted.c_str(), encrypted.length()));
 }
 
-}
+} // namespace rakaly
 
 #endif
-
