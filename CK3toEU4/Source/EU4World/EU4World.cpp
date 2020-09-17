664132177cadab037bc6479978e56cde64fb2ebe
Ivo Elezovic <ivo@elezovic.su>
2020-09-17 20:18:32 +0200

    travis as well.

--- CK3toEU4/Source/EU4World/EU4World.cpp
+++ CK3toEU4/Source/EU4World/EU4World.cpp
@@ -1162,7 +1162,7 @@ void EU4::World::fixTengri()
 	}
 	for (const auto& country: countries)
 	{
-		if (country.second->getTitle())
+		if (!country.second->getTitle())
 		{
 			if (country.second->getReligion() == "tengri_pagan_reformed")
 			{
