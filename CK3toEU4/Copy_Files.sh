6592b2da7d3879e3026877d122adfe0c1692384e
Ivo Elezovic <ivo@elezovic.su>
2020-09-20 01:39:11 +0200

    fixing linux build so it compiles and compiles alone.

--- CK3toEU4/Copy_Files.sh
+++ CK3toEU4/Copy_Files.sh
@@ -6,6 +6,10 @@ cp "Data_Files/FAQ.txt" "Release-Linux/"
 cp "Data_Files/after_converting.txt" "Release-Linux/"
 cp "Data_Files/license.txt" "Release-Linux/"
 
+cp ../imageMagick/lib64/lib*8 "Release-Linux/"
+cp ../imageMagick/lib64/lib*6 "Release-Linux/"
+cp "Resources/librakaly.so" "Release-Linux/"
+
 # Copy configurables
 rm -rf "Release-Linux/configurables"
 cp -r "Data_Files/configurables" "Release-Linux/"
