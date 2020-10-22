7f1a13b58db158110f64ad51081b197621fcf3bf
Ivo Elezović <ivo@elezovic.su>
2020-10-22 09:43:01 +0200

    provinces to new format (#89)

--- CK3toEU4/Source/Mappers/ProvinceMapper/ProvinceMappingsVersion.cpp
+++ CK3toEU4/Source/Mappers/ProvinceMapper/ProvinceMappingsVersion.cpp
@@ -12,6 +12,8 @@ void mappers::ProvinceMappingsVersion::registerKeys()
 {
 	registerKeyword("link", [this](const std::string& unused, std::istream& theStream) {
 		const auto newMapping = std::make_shared<ProvinceMapping>(theStream);
+		if (newMapping->getCK3Provinces().empty() && newMapping->getEU4Provinces().empty())
+			return;
 		mappings.push_back(newMapping);
 	});
 	registerRegex(commonItems::catchallRegex, commonItems::ignoreItem);
