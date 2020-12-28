261277d21b05a3ef0dce0d8462ff14f7fde9ee88
Ivo Elezovic <ivo@elezovic.su>
2020-12-28 22:45:57 +0100

    Fix for missing localized dynasty names in rulers.

--- CK3ToEU4Tests/CK3WorldTests/DynastyTests/HouseTests.cpp
+++ CK3ToEU4Tests/CK3WorldTests/DynastyTests/HouseTests.cpp
@@ -16,6 +16,7 @@ TEST(CK3World_HouseTests, loadValuesDefaultToBlank)
 	const CK3::House house(input, 1);
 
 	ASSERT_TRUE(house.getName().empty());
+	ASSERT_TRUE(house.getLocalizedName().empty());
 	ASSERT_TRUE(house.getPrefix().empty());
 	ASSERT_FALSE(house.getDynasty().first);
 }
@@ -24,12 +25,14 @@ TEST(CK3World_HouseTests, housePrimitivesCanBeLoaded)
 {
 	std::stringstream input;
 	input << "name = \"dynn_Villeneuve\"\n";
+	input << "localized_name = \"Gaye\"\n";
 	input << "prefix = \"dynnp_de\"\n";
 	input << "dynasty = 19\n";
 
 	const CK3::House house(input, 42);
 
 	ASSERT_EQ("dynn_Villeneuve", house.getName());
+	ASSERT_EQ("Gaye", house.getLocalizedName());
 	ASSERT_EQ("dynnp_de", house.getPrefix());
 	ASSERT_EQ(19, house.getDynasty().first);
 }
