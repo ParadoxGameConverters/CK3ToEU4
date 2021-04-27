85b2ff006efcbb4e83d318e99578df39e117b247
Ivo Elezovic <ivo@elezovic.su>
2021-04-27 18:46:15 +0200

    update supported version.

--- CK3toEU4/Source/EU4World/Output/outModFile.cpp
+++ CK3toEU4/Source/EU4World/Output/outModFile.cpp
@@ -5,7 +5,7 @@ std::ostream& EU4::operator<<(std::ostream& output, const ModFile& modFile)
 	output << "name = \"Converted - " << modFile.outname << "\"\n";
 	output << "path = \"mod/" << modFile.outname << "\"\n";
 	output << "normal_or_historical_nations = yes\n";
-	output << "supported_version = \"1.30.*\"\n";
+	output << "supported_version = \"1.31.*\"\n";
 	output << "replace_path = \"history/countries/\"\n";
 	output << "replace_path = \"history/provinces/\"\n";
 	output << "replace_path = \"history/diplomacy/\"\n";
