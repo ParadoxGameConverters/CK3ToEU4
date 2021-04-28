5888a67f749e47676d44d38a406b63dfe2e9029c
Ivo Elezović <ivo@elezovic.su>
2021-04-28 22:38:17 +0200

    rakaly update (#179)

--- CK3toEU4/Source/Helpers/rakaly.h
+++ CK3toEU4/Source/Helpers/rakaly.h
@@ -12,28 +12,53 @@ extern "C" {
 #endif // __cplusplus
 
 /**
- * Melts binary encoded CK3 data into normal plaintext data. The melted buffer will contain utf-8 encoded
- * text.
+ * A non-zero return value indicates an error with the melted buffer
  *
- * Parameters:
+ * A non-zero status code can occur from the following:
  *
- *  - data: Pointer to immutable data that represents the binary data. The data can be:
- *    - autosave save
- *    - ironman save
- *    - binary data
- *  - data_len: Length of the data indicated by the data pointer. It is undefined behavior if the
- *  given length does not match the actual length of the data
+ *  - An early EOF
+ *  - Invalid format encountered
+ *  - Too many close delimiters
  *
- * If an unknown token is encountered and rakaly doesn't know how to convert it to plaintext there
- * are two possible outcomes:
+ * # Safety
  *
- *  - If the token is part of an object's key then key and value will not appear in the plaintext
- *  output
- *  - Else the object value (or array value) will be string of "__unknown_x0$z" where z is the
- *  hexadecimal representation of the unknown token.
+ * Must pass in a valid pointer to a `MeltedBuffer`
  */
-MeltedBuffer *rakaly_ck3_melt(const char *data_ptr,
-                              size_t data_len);
+int rakaly_melt_error_code(const struct MeltedBuffer *res);
+
+/**
+ * Destroys a `MeltedBuffer` once you are done with it.
+ *
+ * # Safety
+ *
+ * Must pass in a valid pointer to a `MeltedBuffer`
+ */
+void rakaly_free_melt(struct MeltedBuffer *res);
+
+/**
+ * Returns the length of the melted data in bytes.
+ *
+ * # Safety
+ *
+ * Must pass in a valid pointer to a `MeltedBuffer`
+ */
+size_t rakaly_melt_data_length(const struct MeltedBuffer *res);
+
+/**
+ * Writes the melted data into a provided buffer that is a given length.
+ *
+ * Returns the number of bytes copied from the melted data to the provided buffer.
+ *
+ * If the buffer is not long enough for the melted data, then 0 is returned.
+ *
+ * If the melted data or provided buffer are null, then 0 is returned.
+ *
+ * # Safety
+ *
+ * - Must pass in a valid pointer to a `MeltedBuffer`
+ * - Given buffer must be at least the given length in size
+ */
+size_t rakaly_melt_write_data(const struct MeltedBuffer *res, char *buffer, size_t length);
 
 /**
  * Melts binary encoded ironman data into normal plaintext data that can be understood by EU4
@@ -56,16 +81,31 @@ MeltedBuffer *rakaly_ck3_melt(const char *data_ptr,
  *  - Else the object value (or array value) will be string of "__unknown_x0$z" where z is the
  *  hexadecimal representation of the unknown token.
  */
-MeltedBuffer *rakaly_eu4_melt(const char *data_ptr, size_t data_len);
+struct MeltedBuffer *rakaly_eu4_melt(const char *data_ptr, size_t data_len);
 
 /**
- * Destroys a `MeltedBuffer` once you are done with it.
+ * Melts binary encoded CK3 data into normal plaintext data. The melted buffer will contain utf-8 encoded
+ * text.
  *
- * # Safety
+ * Parameters:
  *
- * Must pass in a valid pointer to a `MeltedBuffer`
+ *  - data: Pointer to immutable data that represents the binary data. The data can be:
+ *    - autosave save
+ *    - ironman save
+ *    - binary data
+ *  - data_len: Length of the data indicated by the data pointer. It is undefined behavior if the
+ *  given length does not match the actual length of the data
+ *
+ * If an unknown token is encountered and rakaly doesn't know how to convert it to plaintext there
+ * are two possible outcomes:
+ *
+ *  - If the token is part of an object's key then key and value will not appear in the plaintext
+ *  output
+ *  - Else the object value (or array value) will be string of "__unknown_x0$z" where z is the
+ *  hexadecimal representation of the unknown token.
  */
-void rakaly_free_melt(MeltedBuffer *res);
+struct MeltedBuffer *rakaly_ck3_melt(const char *data_ptr,
+                                     size_t data_len);
 
 /**
  * Melts binary encoded Imperator data into normal plaintext data. The melted buffer will contain utf-8 encoded
@@ -87,48 +127,29 @@ void rakaly_free_melt(MeltedBuffer *res);
  *  - Else the object value (or array value) will be string of "__unknown_x0$z" where z is the
  *  hexadecimal representation of the unknown token.
  */
-MeltedBuffer *rakaly_imperator_melt(const char *data_ptr,
-                                    size_t data_len);
+struct MeltedBuffer *rakaly_imperator_melt(const char *data_ptr,
+                                           size_t data_len);
 
 /**
- * Returns the length of the melted data in bytes.
+ * Melts binary encoded HOI4 data into normal plaintext data. The melted buffer will contain utf-8 encoded
+ * text.
  *
- * # Safety
+ * Parameters:
  *
- * Must pass in a valid pointer to a `MeltedBuffer`
+ *  - data: Pointer to immutable data that represents the binary data
+ *  - data_len: Length of the data indicated by the data pointer. It is undefined behavior if the
+ *  given length does not match the actual length of the data
+ *
+ * If an unknown token is encountered and rakaly doesn't know how to convert it to plaintext there
+ * are two possible outcomes:
+ *
+ *  - If the token is part of an object's key then key and value will not appear in the plaintext
+ *  output
+ *  - Else the object value (or array value) will be string of "__unknown_x0$z" where z is the
+ *  hexadecimal representation of the unknown token.
  */
-size_t rakaly_melt_data_length(const MeltedBuffer *res);
-
-/**
- * A non-zero return value indicates an error with the melted buffer
- *
- * A non-zero status code can occur from the following:
- *
- *  - An early EOF
- *  - Invalid format encountered
- *  - Too many close delimiters
- *
- * # Safety
- *
- * Must pass in a valid pointer to a `MeltedBuffer`
- */
-int rakaly_melt_error_code(const MeltedBuffer *res);
-
-/**
- * Writes the melted data into a provided buffer that is a given length.
- *
- * Returns the number of bytes copied from the melted data to the provided buffer.
- *
- * If the buffer is not long enough for the melted data, then 0 is returned.
- *
- * If the melted data or provided buffer are null, then 0 is returned.
- *
- * # Safety
- *
- * - Must pass in a valid pointer to a `MeltedBuffer`
- * - Given buffer must be at least the given length in size
- */
-size_t rakaly_melt_write_data(const MeltedBuffer *res, char *buffer, size_t length);
+struct MeltedBuffer *rakaly_hoi4_melt(const char *data_ptr,
+                                      size_t data_len);
 
 #ifdef __cplusplus
 } // extern "C"
