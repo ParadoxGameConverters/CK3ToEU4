1c0234027b9c06855d4a45d5a532c975dc23da84
Ivo Elezović <ivo@elezovic.su>
2021-09-23 09:59:26 +0200

    update wf (#258)
    * update wf
    
    * update wf
    
    * restore clang
    
    * restore clang

--- build_linux.sh
+++ build_linux.sh
@@ -3,13 +3,6 @@
 export CC=/usr/bin/gcc-11 &&
 export CXX=/usr/bin/g++-11 &&
 
-cd imageMagick &&
-cat im7.10.tar.* > im7.10-linux-source.tar &&
-tar xvf im7.10-linux-source.tar &&
-cd ImageMagick-7.0.10 &&
-./configure --with-quantum-depth=8 --enable-hdri=no --with-x=no --with-utilities=no &&
-sudo make install &&
-cd ../../ &&
 rm -rf Release/ &&
 cd Fronter &&
 ./build_linux.sh &&
@@ -18,7 +11,7 @@ cd ../CK3toEU4 &&
 rm -rf build &&
 rm -rf Release-Linux &&
 cmake -H. -Bbuild &&
-cmake --build build -- -j3 &&
+cmake --build build -- -j40 &&
 mv Release-Linux ../Release/CK3toEU4 &&
 cd .. &&
 
