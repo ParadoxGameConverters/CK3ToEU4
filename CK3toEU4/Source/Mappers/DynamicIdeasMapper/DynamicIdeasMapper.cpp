d648dff3ef3d57ac36a82f9735d721c496decc07
Gawquon <gawquonthestoryteller@gmail.com>
2022-03-11 21:50:46 -0500

    Beginning parser update

--- CK3toEU4/Source/Mappers/DynamicIdeasMapper/DynamicIdeasMapper.cpp
+++ CK3toEU4/Source/Mappers/DynamicIdeasMapper/DynamicIdeasMapper.cpp
@@ -18,18 +18,27 @@ mappers::DynamicIdeasMapper::DynamicIdeasMapper(std::istream& theStream)
 
 void mappers::DynamicIdeasMapper::registerKeys()
 {
-	registerKeyword("ethos", [this](const std::string& unused, std::istream& theStream) {
+	registerKeyword("ethos", [this](std::istream& theStream) {
 		std::vector<std::string> ethosAndEffects = commonItems::getStrings(theStream); // Separates the ethos/tradition from their effects via head/tail op
 		ethosMap.emplace(ethosAndEffects.front(), std::vector<std::string>(ethosAndEffects.begin() + 1, ethosAndEffects.end()));
 	});
-	registerKeyword("tradition", [this](const std::string& unused, std::istream& theStream) {
+	registerKeyword("tradition", [this](std::istream& theStream) {
 		std::vector<std::string> traditionAndEffects = commonItems::getStrings(theStream);
 		traditionMap.emplace(traditionAndEffects.front(), std::vector<std::string>(traditionAndEffects.begin() + 1, traditionAndEffects.end()));
 	});
-	registerKeyword("default", [this](const std::string& unused, std::istream& theStream) {
+	registerKeyword("default", [this](std::istream& theStream) {
 		std::vector<std::string> defaultAndEffects = commonItems::getStrings(theStream);
 		defaultMap.emplace(defaultAndEffects.front(), std::vector<std::string>(defaultAndEffects.begin() + 1, defaultAndEffects.end()));
 		traditionMap.emplace(defaultAndEffects.front(), std::vector<std::string>(defaultAndEffects.begin() + 1, defaultAndEffects.end()));
+	});
+	registerKeyword("link", [this](std::istream& theStream) {
+		DynamicIdeasMapper(theStream);
+	});
+	registerKeyword("eu4", [this](std::istream& theStream) {
+		DynamicIdeasMapper(theStream);
+	});
+	registerKeyword("ck3", [this](std::istream& theStream) {
+		DynamicIdeasMapper(theStream);
 	});
 	registerRegex(commonItems::catchallRegex, commonItems::ignoreItem);
 }
