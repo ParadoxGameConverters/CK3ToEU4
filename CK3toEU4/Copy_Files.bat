226f1c04602d2ac63ffd51fb93da1f69b52c9d94
Ivo Elezovic <ivo.elezovic@gmail.com>
2023-01-23 13:35:19 +0100

    ditch excess fronter files

--- CK3toEU4/Copy_Files.bat
+++ CK3toEU4/Copy_Files.bat
@@ -5,6 +5,7 @@ copy "Data_Files\configuration-example.txt" "..\Release\CK3toEU4\"
 copy "Resources\msvcp140_codecvt_ids.dll" "..\Release\CK3toEU4\"
 copy "Resources\vcruntime140_1.dll" "..\Release\CK3toEU4\"
 copy "Resources\rakaly.dll" "..\Release\CK3toEU4\"
+copy "Data_Files\GeneralFAQ-READ.ME.FIRST.txt" "..\Release\"
 
 mkdir "..\Release\Configuration"
 copy "Data_Files\fronter-configuration.txt" "..\Release\Configuration\"
