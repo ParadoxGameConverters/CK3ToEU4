d62d1439306688ddde42fff2ca70c480c39d8f93
Zemurin <ivo@element.hr>
2023-02-03 15:03:26 +0100

    wf

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#rm -rf build &&
-#rm -rf Release-Linux &&
-#rm -rf test &&
-#rm -rf lib &&
-cmake -H. -Bbuild &&
-cmake --build build -- -j40
--- build_linux_foreign.sh
+++ build_linux_foreign.sh
@@ -1,29 +1,16 @@
 #!/bin/bash
 
-export CC=/usr/bin/gcc-11 &&
-export CXX=/usr/bin/g++-11 &&
-
 cd imageMagick &&
 cat im7.10.tar.* > im7.10-linux-source.tar &&
 tar xvf im7.10-linux-source.tar &&
 cd ImageMagick-7.0.10 &&
 ./configure --with-quantum-depth=8 --enable-hdri=no --with-x=no --with-utilities=no &&
 sudo make install &&
+sudo ldconfig /usr/local/lib &&
 cd ../../ &&
 
-rm -rf Release/ &&
-cd Fronter &&
-./build_linux.sh &&
-mv Release ../ &&
-cd ../CK3toEU4 &&
 rm -rf build &&
 rm -rf Release-Linux &&
+rm -rf test &&
 cmake -H. -Bbuild &&
-cmake --build build -- -j3 &&
-mv Release-Linux ../Release/CK3toEU4 &&
-cd .. &&
-
-cp CK3toEU4/Data_Files/*yml Release/Configuration/ &&
-cp CK3toEU4/Data_Files/fronter*txt Release/Configuration/ &&
-
-tar -cjf CK3toEU4-dev-release.tar.bz2 Release
+cmake --build build -- -j3
